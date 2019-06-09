f50feaf614802e5a7850b8099447782ad1ed561d
Sergey Grishayev <sergey.grishayev@gmail.com>
2019-06-09 18:39:08 +0400

    Избавление от переменных-флагов в публичных методах

--- timeline.go
+++ timeline.go
@@ -19,6 +19,14 @@ type (
 		// Day - начало и конец временной линии (например, рабочий день)
 		Day EventTime
 	}
+
+	windowsType int
+)
+
+const (
+	first windowsType = iota
+	min
+	exactTime
 )
 
 // String - переопределение для вывода времени события в формате hh:mm – hh:mm
@@ -58,9 +66,14 @@ func CreateTL(beginHour, beginMinute, endHour, endMinute int) (TimeLine, error) 
 // hoursBegin, minutesBegin, hoursEnd, minutesEnd - время начала и конца события в часах и минутах
 // doNotMatter - добавлять ли событие, если его время пересекается со временем добавленного ранее события
 // возвращает ошибку, если время пересекается
-func (tl *TimeLine) Add(hoursBegin, minutesBegin, hoursEnd, minutesEnd int, doNotMatter bool) error {
+func (tl *TimeLine) Add(hoursBegin, minutesBegin, hoursEnd, minutesEnd int) error {
 	return tl.addEvent(time2tL(OffsetTime(hoursBegin), OffsetTime(minutesBegin)),
-		time2tL(OffsetTime(hoursEnd), OffsetTime(minutesEnd)), doNotMatter)
+		time2tL(OffsetTime(hoursEnd), OffsetTime(minutesEnd)), false)
+}
+
+func (tl *TimeLine) AddAnyWay(hoursBegin, minutesBegin, hoursEnd, minutesEnd int) error {
+	return tl.addEvent(time2tL(OffsetTime(hoursBegin), OffsetTime(minutesBegin)),
+		time2tL(OffsetTime(hoursEnd), OffsetTime(minutesEnd)), true)
 }
 
 func (tl *TimeLine) addEvent(begin, end OffsetTime, doNotMatter bool) (err error) {
@@ -79,14 +92,39 @@ func (tl *TimeLine) addEvent(begin, end OffsetTime, doNotMatter bool) (err error
 	return
 }
 
+// AddDurationFirst - добавляет событие в первое свободное "окно"
+func (tl TimeLine) AddDurationFirst(duration int) (EventTime, error) {
+	return tl.addDuration(0, 0, duration, first)
+}
+
+// AddDurationMin - добавляет событие в минимальное по размеру свободное "окно"
+func (tl TimeLine) AddDurationMin(duration int) (EventTime, error) {
+	return tl.addDuration(0, 0, duration, min)
+}
+
+// AddDurationExactTime - добавляет событие в точное время и с известной длительностью
+func (tl TimeLine) AddDurationExactTime(beginHour, beginMinute, duration int) (EventTime, error) {
+	return tl.addDuration(beginHour, beginMinute, duration, exactTime)
+}
+
 // AddDuration - добавляет время события в свободное "окно"
 // duration - длительность события в минутах
 // first - признак добавления (true - добавить в первое подходящее "окно", false - добавить в минимальное подходящее "окно"
-func (tl *TimeLine) AddDuration(duration int, first bool) (EventTime, error) {
+func (tl *TimeLine) addDuration(beginH, beginM, duration int, windows windowsType) (EventTime, error) {
 	var (
 		err        error
 		begin, end OffsetTime
 	)
+	if windows == exactTime {
+		beginOffset := time2tL(OffsetTime(beginH), OffsetTime(beginM))
+		if beginOffset >= tl.Day.Begin {
+			begin, end = beginOffset, beginOffset+OffsetTime(duration)
+			err = tl.addEvent(begin, end, false)
+			return EventTime{begin, end}, err
+		} else {
+			return EventTime{}, fmt.Errorf("event starts before timeline begins")
+		}
+	}
 	events := tl.GetEmpty()
 	if len(events) == 0 {
 		return EventTime{}, fmt.Errorf("no free period")
@@ -100,7 +138,7 @@ func (tl *TimeLine) AddDuration(duration int, first bool) (EventTime, error) {
 	for i := range events {
 		tempDuration = events[i].End - events[i].Begin
 		if (tempDuration >= OffsetTime(duration)) && (flag || tempDuration < min) {
-			if first {
+			if windows == first {
 				begin, end = events[i].Begin, events[i].Begin+OffsetTime(duration)
 				err = tl.addEvent(begin, end, false)
 				return EventTime{begin, end}, err
